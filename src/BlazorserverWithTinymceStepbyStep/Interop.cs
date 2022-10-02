d56c5c5d58efa84d34da6a3bd63386edab751e34
iaspnetcore <290996724@qq.com>
2022-10-03 01:49:08 +0800

    Add GetContent()

---
+++ src/BlazorserverWithTinymceStepbyStep/Interop.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace BlazorserverWithTinymceStepbyStep
+{
+    public static class Interop
+    {
+
+        public static ValueTask<string> GetContent(IJSRuntime jSRuntime, string id)
+        {
+         var content=  jSRuntime.InvokeAsync<string>("blazeditorGetContent", id);
+            return content;
+                 
+        }
+            
+
+    }
+}
---
+++ src/BlazorserverWithTinymceStepbyStep/wwwroot/js/tinymce-blazor.js
@@ -0,0 +1,52 @@
+console.log('loading js tinymce-blazor');
+
+
+
+window.blazeditorInit = function (id, option, callback) {
+    var html = document.getElementById(id).innerHTML;
+
+    function setup(ed) {
+        ed.on("init",
+            function () {
+                tinyMCE.get(id).setContent(html);
+                tinyMCE.execCommand('mceRepaint');
+            }
+        );
+
+        ed.on("change", function () {
+            var content = ed.getContent();
+            callback(content);
+        });
+    }
+
+    var config = {
+        selector: 'textarea#' + id,
+        inline: false,
+        setup: setup,
+        default_link_target: '_blank',
+        smart_paste: false,
+    };
+
+    for (var key in option) {
+        if (option.hasOwnProperty(key)) {
+            config[key] = option[key];
+        }
+    }
+
+    if (option.inlineMode) {
+        config.selector = '#' + id;
+        config.inline = true;
+    }
+
+    tinymce.init(config);
+}
+
+
+//获取文档编辑的内容
+window.blazeditorgetContent= function () {
+    const content = tinymce.activeEditor.getContent();
+
+    return content;
+}
+
+
