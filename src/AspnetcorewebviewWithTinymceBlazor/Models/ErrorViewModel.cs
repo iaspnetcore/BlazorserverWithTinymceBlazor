e8429fd0ef465f8c8174e728afba3188d2939188
iaspnetcore <290996724@qq.com>
2022-10-07 22:37:02 +0800

    AspnetcorewebviewWithTinymceBlazor

---
+++ src/AspnetcorewebviewWithTinymceBlazor/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspnetcorewebviewWithTinymceBlazor.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
